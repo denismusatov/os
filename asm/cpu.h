ed720ef9788cb5a7ed08d0a8aed768a16b1895e2
Denis Musatov <d.s.musatov@gmail.com>
2017-02-19 14:54:47 +0700

    Removed .c files in 'asm' folder. Moved the implementation to .h files to facilitate inlining

--- asm/cpu.h
+++ asm/cpu.h
@@ -4,11 +4,46 @@
 #include <stdint.h>
 #include <stdbool.h>
 
-uint32_t get_cr0_value();
-uint32_t get_cr2_value();
-uint32_t get_cr3_value();
+inline uint32_t get_cr0_value()
+{
+    uint32_t cr0;
+    __asm__ __volatile__ (
+            "mov %%cr0, %%eax\n\t"
+            "mov %%eax, %0\n\t"
+             : "=m" (cr0)
+             : /* no input */
+             : "%eax");
+    return cr0;
+}
 
-bool protected_mode_is_enabled();
+inline uint32_t get_cr2_value()
+{
+    uint32_t cr2;
+    __asm__ __volatile__ (
+            "mov %%cr2, %%eax\n\t"
+            "mov %%eax, %0\n\t"
+             : "=m" (cr2)
+             : /* no input */
+             : "%eax");
+    return cr2;
+}
+
+inline uint32_t get_cr3_value()
+{
+    uint32_t cr3;
+    __asm__ __volatile__ (
+            "mov %%cr3, %%eax\n\t"
+            "mov %%eax, %0\n\t"
+             : "=m" (cr3)
+             : /* no input */
+             : "%eax");
+    return cr3;
+}
+
+inline bool in_protected_mode();
+{
+    return get_cr0_value() & 0x1;
+}
 
 #endif
 
